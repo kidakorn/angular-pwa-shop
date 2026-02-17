e96d423a258151964ff508fda22beffcd881b88d
KIDAKORN.I <kidakorn.1@gmail.com>
2026-02-18 00:03:05 +0700

    'api-render'

--- src/app/services/auth.ts
+++ src/app/services/auth.ts
@@ -11,7 +11,7 @@ export class AuthService {
   private http = inject(HttpClient);
   private router = inject(Router);
 
-  private apiUrl = 'https://angular-pwa-shop.onrender.com/';  // http://localhost:3000/api
+  private apiUrl = 'https://angular-pwa-shop.onrender.com/api';  // http://localhost:3000/api
   currentUser = signal<any>(null);
 
   constructor() {
