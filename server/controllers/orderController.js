6d446281c14d2ac16328bc71783fce193990985a
KIDAKORN.I <kidakorn.1@gmail.com>
2026-02-21 00:43:31 +0700

    'updateCount-Stock'

--- server/controllers/orderController.js
+++ server/controllers/orderController.js
@@ -4,7 +4,7 @@ const Order = require('../models/Order');
 exports.createOrder = async (req, res) => {
 	try {
 		const { items, shippingAddress, paymentDetails } = req.body;
-		console.log('ข้อมูลผู้ใช้ที่แกะจาก Token:', req.user);
+		// ดึง userId จาก Token (ตามที่เราแก้กันไปเมื่อวาน)
 		const userId = req.user._id || req.user.id;
 
 		let calculatedTotal = 0;
@@ -13,14 +13,26 @@ exports.createOrder = async (req, res) => {
 		for (let item of items) {
 			const product = await Product.findById(item.productId);
 
-			if(product) {
-				calculatedTotal += ( product.price * item.quantity );
+			if (product) {
+				if (product.stock < item.quantity) {
+					return res.status(400).json({
+						error: `ขออภัย สินค้า "${product.title}" มีจำนวนไม่พอ (เหลือเพียง ${product.quantity} ชิ้น)`
+					});
+				}
+
+				calculatedTotal += (product.price * item.quantity);
 
 				finalItems.push({
 					productId: product._id,
 					quantity: item.quantity,
 					price: product.price,
 				});
+
+				product.stock -= item.quantity;
+				
+				product.soldCount += item.quantity;
+
+				await product.save();
 			}
 		}
 
